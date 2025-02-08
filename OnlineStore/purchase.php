cf74ee2d7af801bd5e48890ec7d363d031c6a332
Morsalin Miah <info.morsalin.miah@gmail.com>
2025-02-08 17:45:20 +0600

    Update purchase.php

--- OnlineStore/purchase.php
+++ OnlineStore/purchase.php
@@ -65,7 +65,7 @@
     </form>
 </div>
 <footer class="text-center text-gray-500 mt-8">
-    &copy; <?php echo date("Y"); ?> Ornab Biswass. All rights reserved.
+    &copy; <?php echo date("Y"); ?> Ornab Biswass, Morsalin, Sahadat, Rakib. All rights reserved.
 </footer>
 
 
